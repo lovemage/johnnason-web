ee1cd52205020e023712dc12f0d14af05e35b636
lovemage <lovemage@gmail.com>
2025-03-30 20:51:25 +0800

    feat: add custom favicon and update page title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -49,7 +49,7 @@ const Home: React.FC = () => {
       category: 'Raw Materials',
       link: '/raw-materials'
     },
-    {htt
+    {
       id: 3,
       image: 'https://images.unsplash.com/photo-1602223114290-ba6de2938acc?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       title: 'Precision Components',
